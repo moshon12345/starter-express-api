4df66111dda5e241e37b72d5d066cfdfd77115d3
moshon12345 <107131721+moshon12345@users.noreply.github.com>
2023-04-22 22:24:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
+
+app.get('/TESTING3', router.get('/TESTING3', function(req, res) {
+    const dataStream = got.stream({
+        url: 'https://catfact.ninja/fact',
+    });
+    pipeline(dataStream, res, (err) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        // console.log("lugi")
+    });
+  })); 
+
+  module.exports = router;
