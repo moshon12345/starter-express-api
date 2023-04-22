9c67bcef3a4b57d8a8f97cb74414ef10987cf89f
moshon12345 <107131721+moshon12345@users.noreply.github.com>
2023-04-22 22:29:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,36 @@ const blugi = {
     "name": "Blugi"
 }
 
+app.get('/TESTING', sendBackData); 
 app.get('/TESTING2', sendBackData2);
+
 
 function sendBackData2(req, res) {
     res.send(blugi)
 }
+
+function sendBackData(req, res) {
+        // res.send(blugi)
+        let url = "https://catfact.ninja/fact";
+
+        fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+
+                // this.ruki = data;
+                res.send(data)
+            })
+
+            .catch((error) => {
+                console.error("Error:", error);
+
+            });
+        }
 
 app.listen(process.env.PORT || 3000)
 
